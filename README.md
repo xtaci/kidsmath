8bd14ee312afd6afe7308477788eacb7476efd1e
xtaci <daniel820313@gmail.com>
2023-12-13 11:09:39 +0800

    readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # kidsmath
 a simple program to generate math quizs for my kid.
+
+```
+./kidsmath basic add --maxdev 50 --pattern "500x100"
+2023/12/13 11:09:18 N=500, M=100
+295 + 85 =
+308 + 86 =
+446 + 68 =
+280 + 50 =
+353 + 95 =
+409 + 63 =
+391 + 70 =
+498 + 65 =
+306 + 77 =
+288 + 95 =
+...
+```
--- cmd/root.go
+++ cmd/root.go
@@ -15,14 +15,9 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "priv",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "kidsmath",
+	Short: "A simple program to generate math quizs",
+	Long:  `Generate multiplcation, substraction, addition, division quizs within a given range.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
